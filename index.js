a3b4e6818ffe51ff5de96d19104c1f2677555d5d
Gauravsingh0712 <sumitgaurav6295@gmail.com>
2024-04-08 23:18:19 +0530

    implemented login controller

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+//Loads .env file contents into process.env by default
 require("dotenv").config()
 
 const PORT = process.env.PORT || 4000;
@@ -10,9 +11,11 @@ app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
 
+//Returns middleware that only parses json and only looks at requests where the Content-Type header matches the type option.
 app.use(express.json());
 
-require("./config/database").connect();
+const dbConnect = require("./config/database")
+dbConnect.connect()
 
 // Routes and mounting
 const user = require("./routes/user");
