4fc21fe2e2709e2cee24956d61cc4b573b613a81
Gauravsingh0712 <sumitgaurav6295@gmail.com>
2024-04-09 23:24:10 +0530

    Added middlewares

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,34 @@ const express = require("express");
 const router = express.Router();
 
 const { login, signup } = require("../controllers/auth");
+const { auth, isStudent, isAdmin } = require("../middlewares/auth");
+
 
 router.post("/login", login);
 router.post("/signup", signup);
+
+//testing route for single middleware
+router.get("/test", auth, (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Welcome to protected route of test"
+    })
+})
+
+//protected routes
+
+router.get("/student", auth, isStudent, (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Welcome to protected route of student"
+    })
+});
+
+router.get("/admin", auth, isAdmin, (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Welcome to protected route of admin"
+    })
+})
 
 module.exports = router
